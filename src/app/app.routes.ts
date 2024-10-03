e98121a10eeef3ffbd4ad6f5ac43703883e50be9
Miguel Angel <u202220294@upc.edu.pe>
2024-10-03 04:54:08 -0500

    chore: configured de routes of the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./public/pages/home/home.component";
+import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
+import { BundleInformationComponent } from "./store/pages/bundle-information/bundle-information.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'bundles/:bundleId/products', component: BundleInformationComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
