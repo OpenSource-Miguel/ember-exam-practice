f79f98135aaae2a7a99344dfc8675878205aae54
Miguel Angel <u202220294@upc.edu.pe>
2024-10-03 04:53:50 -0500

    chore: added pages of the proyect

---
+++ src/app/store/pages/bundle-information/bundle-information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bundle-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './bundle-information.component.html',
+  styleUrl: './bundle-information.component.css'
+})
+export class BundleInformationComponent {
+
+}
